2938f4bd3db5f05aee87be0fc1153fa838a2a996
Cameron Scott <camscottgreen@hotmail.com>
2019-08-25 10:33:14 +1000

    Add guess functionality

--- mastermind.rb
+++ mastermind.rb
@@ -12,4 +12,30 @@ class Mastermind
         @board = @board.map {PEGS.sample}
     end
 
+    def guess(guess)
+        bulls = 0
+        cows = 0
+        copy = @board.dup
+
+        guess.each_with_index do |peg, index|
+            if @board[index] == peg
+                bulls += 1
+                copy[index] = nil
+                guess[index] = nil
+            end
+        end
+
+        copy.compact!
+        guess.compact!
+
+        guess.each do |peg|
+            if copy.include?peg
+                cows += 1
+                copy.delete_at(copy.index(peg))
+            end
+        end
+
+        return "Bulls: #{bulls} Cows: #{cows}"
+    end
+
 end
